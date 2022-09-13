7227ca9d422de817f89f7d9d70101119b0024928
youngtaek <youngtaek@peoplev.co.kr>
2022-09-13 15:40:10 +0900

    Initial Commit

---
+++ RTCExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RTCExample
+//
+//  Created by young on 2022/09/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
